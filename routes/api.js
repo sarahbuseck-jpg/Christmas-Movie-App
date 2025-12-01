cf43b1e2a77b102bf8fe84f2a58a90b252194f10
Sarah Buseck <sarahbuseck@mscodingacademies.org>
2025-11-30 20:27:16 -0600

    commit change

--- routes/api.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', (req, res) => {
-  res.send('christmasdb25 api');
-});
-
-
-module.exports = router;
--- server.js
+++ server.js
@@ -1,21 +1,20 @@
-const express = require('express');   // Only declare once
+const express = require('express');   
 const server = express();
 const cors = require('cors');
 const helmet = require('helmet');
-const apiRouter = require('./routes/api'); // make sure the file exists
+const apiRouter = require('./routes/api'); 
 
 const PORT = process.env.PORT || 3000;
 
-// Middleware
+
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
 
-// Routes
+
 server.use('/api', apiRouter);
 
-// Start server
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
