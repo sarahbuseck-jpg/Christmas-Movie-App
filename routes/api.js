f464409e9034484154a93edf35dbc82e0a94d334
Sarah Buseck <sarahbuseck@mscodingacademies.org>
2025-11-30 22:18:28 -0600

    commit changes

--- routes/api.js
+++ routes/api.js
@@ -1,20 +1,20 @@
-// routes/api.js
 const express = require('express');
 const router = express.Router();
+const PORT = process.env.PORT || 3000
 
-// GET /api
-router.get('/', (req, res) => {
-  res.send('christmasdb25');
-});
 
-// Example GET route
-router.get('/hello', (req, res) => {
-  res.json({ message: 'Hello from ChristmasDB!' });
-});
+router.get('/api', (req, res) => {
+ // res.send('christmasdb25');
+res.json({
+   'All Actors': `http://localhost:${PORT}/api/actors`
+ })
+})
 
-// Example POST route
-router.post('/echo', (req, res) => {
-  res.json({ youSent: req.body });
-});
+router.use((req, res, next)=> {
+    res.status(404)
+    .send('<h1>404 Error this page does not exist')
+})
+
+
 
 module.exports = router;
