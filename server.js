86c4dce962a4f41aa5e134138e5d754ca72637b4
Sarah Buseck <sarahbuseck@mscodingacademies.org>
2025-12-10 09:58:00 -0600

    commit changes

--- server.js
+++ server.js
@@ -1,22 +1,33 @@
 const express = require("express");
 const path = require("path");
-const db = require('./config/dbconfig');
+const db = require("./config/dbconfig");
+
 const app = express();
 
+// =============================
 // MIDDLEWARE
+// =============================
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+
+// Body parser
 app.use(express.urlencoded({ extended: true }));
+
+// Public folder (static files: CSS, JS, images, etc.)
 app.use(express.static(path.join(__dirname, "public")));
 
+
+// =============================
 // HOME PAGE
+// =============================
 app.get("/", (req, res) => {
     res.render("home");
 });
 
-/* ============================
-      ACTORS ROUTES
-============================ */
+
+// =============================
+// ACTORS ROUTES
+// =============================
 app.get("/actors", async (req, res) => {
     const [actors] = await db.query("SELECT * FROM actors");
     res.render("actor/list", { actors });
@@ -28,64 +39,70 @@ app.get("/actors/add", (req, res) => {
 
 app.post("/actors/add", async (req, res) => {
     const { first_name, last_name } = req.body;
-    await db.query("INSERT INTO actors(first_name, last_name) VALUES (?, ?)", [
-        first_name,
-        last_name
-    ]);
+    await db.query(
+        "INSERT INTO actors(first_name, last_name) VALUES (?, ?)",
+        [first_name, last_name]
+    );
     res.redirect("/actors");
 });
 
-/* ============================
-     DIRECTORS ROUTES
-============================ */
+
+// =============================
+// DIRECTORS ROUTES
+// =============================
 app.get("/directors", async (req, res) => {
     const [directors] = await db.query("SELECT * FROM directors");
     res.render("directors/list", { directors });
 });
 
-/* ============================
-        GENRES ROUTES
-============================ */
+
+// =============================
+// GENRES ROUTES
+// =============================
 app.get("/genres", async (req, res) => {
     const [genres] = await db.query("SELECT * FROM genres");
     res.render("genres/list", { genres });
 });
 
-/* ============================
-       PROGRAM ROUTES
-============================ */
+
+// =============================
+// PROGRAM ROUTES
+// =============================
 app.get("/programs", async (req, res) => {
     const [programs] = await db.query("SELECT * FROM program");
     res.render("programs/list", { programs });
 });
 
-/* ============================
-      STREAMING ROUTES
-============================ */
 
+// =============================
+// STREAMING ROUTES
+// =============================
 app.get("/streamings", async (req, res) => {
     const [streamings] = await db.query("SELECT * FROM streamings");
     res.render("streamings/list", { streamings });
 });
-/* ============================
-     PRODUCTION ROUTES
-============================ */
+
+
+// =============================
+// PRODUCTION ROUTES
+// =============================
 app.get("/productions", async (req, res) => {
     const [productions] = await db.query("SELECT * FROM productions");
     res.render("productions/list", { productions });
 });
 
 
-
-
-/* ============================
-         404 ERROR
-============================ */
+// =============================
+// 404 PAGE
+// =============================
 app.use((req, res) => {
     res.status(404).render("errors/404");
 });
 
+
+// =============================
 // SERVER LISTEN
+// =============================
 app.listen(3000, () => {
     console.log("Server running at http://localhost:3000");
 });
